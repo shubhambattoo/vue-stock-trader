38b8b4eae1ab8bbf4f277df2b9daf63feafd6cfb
Shubham Battoo <shubham.battoo@hotmail.com>
2019-09-02 21:55:55 +0530

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="container">
+    <Header />
     <router-view></router-view>
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+};
+</script>
+
 <style>
+body {
+  padding: 30px;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,48 @@
 <template>
-  <header>
-    The header component
+  <header class="mb-3">
+    <nav class="navbar navbar-expand-lg rounded border navbar-light bg-light">
+      <div class="container">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+        <ul class="navbar-nav mr-auto">
+          <router-link active-class="active" to="/portfolio" tag="li">
+            <a class="nav-link">Portfolio</a>
+          </router-link>
+          <router-link active-class="active" to="/stocks" tag="li">
+            <a class="nav-link">Stocks</a>
+          </router-link>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >Save & Load</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Save</a>
+              <a class="dropdown-item" href="#">Load</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-
-}
+  name: "app-header"
+};
 </script>
 
 <style>
-
 </style>
